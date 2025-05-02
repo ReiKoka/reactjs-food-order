ebf960f3d0d0e0b7c0dd3254186eb7c5bd41bbdd
ReiKoka <reikoka1@outlook.com>
2025-05-02 16:46:23 +0200

    merged cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import CartProvider from "./store/CartProvider";
